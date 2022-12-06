277db247c085c5e40833bbf1dbadc1a4d95f5314
Wazy42 <lvocanson@gaming.tech>
2022-12-06 10:55:00 +0100

    Added const file

---
+++ Const.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+// Assets path
+#define ASSETS_PATH "assets/"
+
+// Fonts path
+#define FONT_PATH ASSETS_PATH ## "fonts/"
+#define FONT_SPINWERARD FONT_PATH ## "spinwerad.ttf"
+#define FONT_SPINWERARD_CAPITALIZE FONT_PATH ## "spinweradC.ttf"
+
+// Textures path
+#define TEXTURE_PATH ASSETS_PATH ## "textures/"
+#define TEXTURE_CAT TEXTURE_PATH ## "cat.jpg"
+
+// Sounds path
+#define SOUND_PATH ASSETS_PATH ## "sounds/"
+
+// Sizes
+#define SIZE_TILE 32
